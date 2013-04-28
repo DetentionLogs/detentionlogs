02a07ce28f4fa1fbef861356fd8261592d05bfdb
Bryan McLeod <bryan@bryanmcleod.com.au>
2013-04-28 20:02:00 +1000

    tests

---
+++ app/controllers/gateway_controller.rb
@@ -0,0 +1,10 @@
+class GatewayController < ApplicationController
+ 
+   
+  # GET /incidents
+  # GET /incidents.json
+  def index
+    render :layout => false
+  end
+  
+end
---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,31 @@
+class SubscriptionsController < ApplicationController
+
+  # POST /subscriptions
+  # POST /subscriptions.json
+  def create
+    
+    @subscription = Subscription.new(params[:subscription])
+
+    respond_to do |format|
+      if @subscription.save
+        format.html { redirect_to :back, notice: "Thank you for subscribing" }
+        format.json { render json: @subscription, status: :created, location: @subscription }
+      else
+        format.html { redirect_to :back, notice: "Please enter a valid email" }
+      end
+    end
+  end
+  
+
+  # DELETE /subscriptions/1
+  # DELETE /subscriptions/1.json
+  def destroy
+    @subscription = Subscription.find(params[:id])
+    @subscription.destroy
+
+    respond_to do |format|
+      format.html { redirect_to subscriptions_url }
+      format.json { head :no_content }
+    end
+  end
+end
