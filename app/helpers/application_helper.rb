ee7e1d061ed2ecead1d65e7bc927c6611b981996
Luke Bacon <luke@equivalentideas.com>
2013-08-18 16:45:51 +1000

    helper for titles and descriptions
    based this stake overflow answer http://stackoverflow.com/questions/3059704/rails-3-ideal-way-to-set-title-of-pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
-
+	def title(page_title)
+	  content_for :title, page_title.to_s
+	end
+	
+	def description(page_description)
+	  content_for :description, page_description.to_s
+	end
+	
 end
