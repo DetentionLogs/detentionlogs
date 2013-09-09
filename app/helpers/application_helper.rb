fd2a82437311bf1a4b59a22c17020be90e035072
Luke Bacon <luke@equivalentideas.com>
2013-09-09 21:59:56 +1000

    explorer results helper. Thanks BJ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,16 @@ module ApplicationHelper
 	def description(page_description)
 	  content_for :description, page_description.to_s
 	end
+
+	def incidentexplorer_results
+      type_of_incident = 'incident report'
+      if params[:incident_type]
+        type_of_incident = "#{params[:incident_type]} #{type_of_incident}"
+      end
+      title = pluralize(@incidents.total_count, type_of_incident)
+      title += " from #{@location_name}" if @location_name
+      title += " between "
+      title += [@start_date, @end_date].map{|d| d.strftime("%d %b %Y") }.to_sentence
+	end
 	
 end
