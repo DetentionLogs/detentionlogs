6344ef446727d2927813b4f34ecbc5d2505b1cd7
Luke Bacon <luke@equivalentideas.com>
2014-06-26 18:31:33 +1000

    fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Detentionlogs::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET']
-      :access_key_id => ENV['S3_ACCESS_KEY_ID']
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     }
   }
