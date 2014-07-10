2c3a34fc141370385e7f2a7ae337cb682f97a5d8
Luke Bacon <luke@equivalentideas.com>
2014-07-10 20:22:30 +1000

    program count of incidents and dates for the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
   before_filter :authenticate_admin!, except: [:frontpage]
 
   def frontpage
+    @incident_count = Incident.count
+    @incident_start_date = Incident.order("occured_on").first
+    @incident_end_date = Incident.order("occured_on").last
   end
 end
