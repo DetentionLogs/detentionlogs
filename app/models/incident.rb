7e69027a68036b239d4c684b4f6d12317a760754
Henare Degan <henare.degan@gmail.com>
2014-07-19 16:42:44 +1000

    Group class methods

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -13,29 +13,31 @@ class Incident < ActiveRecord::Base
   }
   scope :by_detailed_report, -> { where('detailed_report_file_name is not null')  }
 
-  def self.default_period_range
-    # TODO: pull from db
-    ('2009-10-03'.to_date)..('2011-05-26'.to_date)
-  end
+  class << self
+    def default_period_range
+      # TODO: pull from db
+      ('2009-10-03'.to_date)..('2011-05-26'.to_date)
+    end
 
-  # Returns a summary of the FOI data for each incident. This should provide
-  # enough info to know if an FOI request has already been made or if
-  # detailed info has already been obtained
-  def self.foi_summary
-    columns = [arel_table[:incident_number], arel_table[:detailed_report_file_name], arel_table[:occured_on]]
-    reqs = FoiRequest.arel_table
-    req_count = Arel::Nodes::NamedFunction.new('string_agg', [reqs[:url_title], ','], 'requests')
+    # Returns a summary of the FOI data for each incident. This should provide
+    # enough info to know if an FOI request has already been made or if
+    # detailed info has already been obtained
+    def foi_summary
+      columns = [arel_table[:incident_number], arel_table[:detailed_report_file_name], arel_table[:occured_on]]
+      reqs = FoiRequest.arel_table
+      req_count = Arel::Nodes::NamedFunction.new('string_agg', [reqs[:url_title], ','], 'requests')
 
-    select([columns, req_count])
-      .joins(Arel::Nodes::OuterJoin.new(reqs, Arel::Nodes::On.new(reqs[:incident_id].eq(arel_table[:id]))))
-      .group(columns)
-  end
+      select([columns, req_count])
+        .joins(Arel::Nodes::OuterJoin.new(reqs, Arel::Nodes::On.new(reqs[:incident_id].eq(arel_table[:id]))))
+        .group(columns)
+    end
 
-  def self.first_occured
-    order("occured_on").first.occured_on if order("occured_on").first
-  end
+    def first_occured
+      order("occured_on").first.occured_on if order("occured_on").first
+    end
 
-  def self.last_occured
-    order("occured_on").last.occured_on if order("occured_on").first
+    def last_occured
+      order("occured_on").last.occured_on if order("occured_on").first
+    end
   end
 end
