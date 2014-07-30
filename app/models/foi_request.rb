0add153daf365de4adf88cbbd9e10dd55575d683
Luke Bacon <luke@equivalentideas.com>
2014-07-30 17:09:28 +1000

    add comment linking to Right To Know api instructions for this model

--- app/models/foi_request.rb
+++ app/models/foi_request.rb
@@ -1,5 +1,7 @@
+# This model is mostly populated through the Right To Know api
 class FoiRequest < ActiveRecord::Base
   belongs_to :incident
+  # see explanation of :display_status https://www.righttoknow.org.au/advancedsearch#statuses
   attr_accessible :described_state, :display_status, :url_title
   # Allow these to be set to match the external request
   attr_accessible :created_at, :updated_at
