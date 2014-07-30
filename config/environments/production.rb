5e7eabfb993d1cfaf5831223e0a0222da2043115
Luke Bacon <luke@equivalentideas.com>
2014-07-30 12:34:44 +1000

    add s3 hostname for aus region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,8 @@ Detentionlogs::Application.configure do
       :bucket => ENV['S3_BUCKET'],
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_host_name => 's3-ap-southeast-2.amazonaws.com'
   }
 
   # Log the query plan for queries taking more than this (works
