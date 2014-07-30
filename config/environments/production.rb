c1e71c6c4d609991595766ee5c5ede009278513b
Luke Bacon <luke@equivalentideas.com>
2014-07-30 12:09:54 +1000

    change paperclip s3 url construction to domain_style

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Detentionlogs::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :url => ":s3_domain_url",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET'],
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
